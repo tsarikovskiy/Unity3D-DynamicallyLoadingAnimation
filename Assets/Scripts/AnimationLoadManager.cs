b75e106642fb73587f0003b2fa62ad4b48ef5a2b
Serg Tsarikovskiy <nonehill@mail.ru>
2015-04-09 13:04:05 +0300

    Repository created

---
+++ Assets/Scripts/AnimationLoadManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationLoadManager : MonoBehaviour 
+{
+	AnimatorOverrideController overrideController;
+	static string prevLoadAnim;
+	Animator animator;
+	ResourceRequest request;
+	AnimatorStateInfo[] layerInfo;
+
+	// Use this for initialization
+	void Start() 
+	{
+		animator	       = GetComponent<Animator>();
+		overrideController = animator.runtimeAnimatorController as AnimatorOverrideController;
+	}
+
+	IEnumerator LoadAnimClip(string clipName, string boolName)
+	{
+		request = Resources.LoadAsync("Animations/" + clipName);
+
+		yield return request;
+
+		AnimationClip animClip = request.asset as AnimationClip;
+		
+		overrideController[clipName + " Empty"] = animClip;
+
+		// Push back state
+		for (int i = 0; i < animator.layerCount; i++)
+		{
+			animator.Play(layerInfo[i].nameHash, i, layerInfo[i].normalizedTime);
+		}
+
+		// Force an update
+		animator.Update(0.0f);
+
+//		animator.SetBool(boolName, true);
+	}
+
+	/// <summary>
+	/// Load Animation Clip
+	/// </summary>
+	 
+	public void LoadAnimation(string animationName, string animationBoolName)
+	{
+		animator = GetComponent<Animator>();
+
+		//Save current state
+		layerInfo = new AnimatorStateInfo[animator.layerCount];
+		
+		for (int i = 0; i < animator.layerCount; i++)
+		{
+			layerInfo[i] = animator.GetCurrentAnimatorStateInfo(i);
+		}
+
+		// Start Load Animation Clip
+		StartCoroutine(LoadAnimClip(animationName, animationBoolName));
+
+		prevLoadAnim = animationName;
+	}	
+	
+	public void UnloadPreviousLoadAnimation()
+	{
+		for (int i = 0; i < animator.layerCount; i++)
+		{
+			layerInfo[i] = animator.GetCurrentAnimatorStateInfo(i);
+		}
+		
+		overrideController[prevLoadAnim + " Empty"] = null;
+		Resources.UnloadUnusedAssets();
+		
+		for (int i = 0; i < animator.layerCount; i++)
+		{
+			animator.Play(layerInfo[i].nameHash, i, layerInfo[i].normalizedTime);
+		}
+		
+		// Force an update
+		animator.Update(0.0f);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	AnimationLoadManager animationLoadManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animationLoadManager = GetComponent<AnimationLoadManager>();
+
+		//Load animation clip after 3 seconds
+		Invoke("LoadAnimataionClip", 3);
+
+		//Unload Animation Clip after 10 seconds
+		Invoke("UnloadAnimationCLip", 10);
+	}
+	
+	void LoadAnimataionClip () 
+	{
+		animationLoadManager.LoadAnimation("Run", null);
+	}
+
+	void UnloadAnimationCLip()
+	{
+		animationLoadManager.UnloadPreviousLoadAnimation();
+	}
+}
